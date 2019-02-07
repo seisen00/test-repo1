c9935e608ab53183a61e3e459cf1a2588579e6b6
Andrey Belousov <a.belousov@bpcbt.com>
2019-02-08 02:43:28 +0300

    added Idea, Firefox support

--- src/test/java/training/selenium/Test1.java
+++ src/test/java/training/selenium/Test1.java
@@ -5,19 +5,33 @@ import org.junit.Test;
 import org.junit.After;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class Test1 {
+    private boolean isChrome = true;
+
     public WebDriver driver;
     public WebDriverWait wait;
+    private String title;
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
+        // isChrome = false;
+        // System.setProperty("webdriver.chrome.driver", "/data/bin/webdriver/chromedriver");
+        // System.setProperty("webdriver.gecko.driver", "/data/bin/webdriver/geckodriver");
+        
+        title = "webdriver - Поиск в Google";
+        if (isChrome) {
+            driver = new ChromeDriver();
+        }
+        else {
+            driver = new FirefoxDriver();
+            title += " - Mozilla Firefox";
+        }
         wait = new WebDriverWait(driver, 10);
     }
 
@@ -27,7 +41,7 @@ public class Test1 {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElement(By.name("q")).sendKeys("webdriver");
         driver.findElement(By.name("btnK")).click();
-        wait.until(titleIs("webdriver - Поиск в Google"));
+        wait.until(titleIs(title));
     }
 
     @After
